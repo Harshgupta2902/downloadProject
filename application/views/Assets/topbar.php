1ae5e42dbe41d1f9726fd6a053c9e497e511f148
Harshgupta2902 <147129512+Harshgupta2902@users.noreply.github.com>
2023-12-08 17:53:02 +0530

    css changed and tabs section margin removed

--- application/views/Assets/topbar.php
+++ application/views/Assets/topbar.php
@@ -11,7 +11,7 @@
                         <path d="M87 68.732l9.926 17.143 29.893-51.59L174.15 116H194L126.817 0z" />
                     </g>
                 </svg>
-                <span class="ml-3 text-xl text-gray-800">Landmark<span class="text-pink-500">.</span></span>
+                <span class="ml-3 text-xl text-gray-800">GetItFast<span class="text-pink-500">.</span></span>
             </a>
 
             <nav id="nav" class="absolute top-0 left-0 z-50 flex flex-col items-center justify-between hidden w-full h-64 pt-5 mt-24 text-sm text-gray-800 bg-white border-t border-gray-200 md:w-auto md:flex-row md:h-24 lg:text-base md:bg-transparent md:mt-0 md:border-none md:py-0 md:flex md:relative">
@@ -62,3 +62,9 @@
 
         </div>
     </header>
+
+    <style>
+        .g-grid-container{
+                margin-left: auto;
+        }
+    </style>    
